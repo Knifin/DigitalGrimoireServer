f3fbf26f0c61bf0111b207747e6eee8629507d40
Knifin <angie.t.seattle@gmail.com>
2024-08-30 13:16:31 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const io = require('socket.io')(3000, {
+    cors: {
+        origin: ['https://app.playclocktower.com'],
+        methods: ["GET"],
+    },
+})
+
+io.on('connection', socket => {
+    socket.on('player-join', (lobbyId,playerId,name,pronouns,role) => {
+        if (lobbyId !== '') {
+            console.log(lobbyId,playerId,name,pronouns,role);
+            socket.to(lobbyId).emit('player-join-info', {lobbyId,playerId,name,pronouns,role});
+        }
+    });
+    socket.on('join-lobby', lobbyId => {
+        socket.join(lobbyId);
+    });
+    socket.on('report-resolution', (height,width) => {
+       console.log(height,width);
+    });
+    socket.on('show-player', (playerId, role) => {
+        console.log('show-player', playerId, role);
+        socket.to(playerId).emit('get-role', role);
+    });
+});
