ab563c7209ebb07218abfd19e42c9f635f4c3fc6
Knifin <angie.t.seattle@gmail.com>
2024-09-01 13:12:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(process.env.PORT || 3000, {
         origin: "https://app.shinpostudios.com",
         methods: ["GET"],
         methods: ["GET"],
-    },
+    }
 });
 
 io.on('connection', socket => {
@@ -13,20 +13,18 @@ io.on('connection', socket => {
             console.log(lobbyId,playerId,name,pronouns,role);
             socket.to(lobbyId).emit('player-join-info', {lobbyId,playerId,name,pronouns,role});
         }
-        }
     });
+    
     socket.on('join-lobby', lobbyId => {
         socket.join(lobbyId);
-        socket.join(lobbyId);
     });
+    
     socket.on('report-resolution', (height,width) => {
        console.log(height,width);
-       console.log(height,width);
     });
+    
     socket.on('show-player', (playerId, role) => {
         console.log('show-player', playerId, role);
-        console.log('show-player', playerId, role);
-        socket.to(playerId).emit('get-role', role);
         socket.to(playerId).emit('get-role', role);
     });
 });
