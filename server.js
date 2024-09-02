b8f5591fd313e85f131eb421c0ee2da70677a334
Knifin <angie.t.seattle@gmail.com>
2024-09-01 22:32:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,31 +14,11 @@ const io = new Server(httpServer, {
   cors: {
         origin: "https://app.shinpostudios.com",
         methods: ["GET"]
-  },
+  }
 });
 
 io.on("connection", (socket) => {
     socket.emit("hello", "Hello World from joinapp.shinpostudios.com!");
-  
-    socket.on('player-join', (lobbyId,playerId,name,pronouns,role) => {
-        if (lobbyId !== '') {
-            console.log(lobbyId,playerId,name,pronouns,role);
-            socket.to(lobbyId).emit('player-join-info', {lobbyId,playerId,name,pronouns,role});
-        }
-    });
-    
-    socket.on('join-lobby', lobbyId => {
-        socket.join(lobbyId);
-    });
-    
-    socket.on('report-resolution', (height,width) => {
-       console.log(height,width);
-    });
-    
-    socket.on('show-player', (playerId, role) => {
-        console.log('show-player', playerId, role);
-        socket.to(playerId).emit('get-role', role);
-    });
 });
 
 httpServer.listen(5000);
