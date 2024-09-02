b216f2d8dab3d7ef631d703e547e62404dcc265e
Knifin <angie.t.seattle@gmail.com>
2024-09-02 13:45:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+io.on("connection", (socket) => {
+    socket.emit("hello", "Hello World from the Server!");
+});
+
 app.use(cors());
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile('./index.html');
 });
 
 httpServer.listen(5000);
