5ea6ec5560cac20fffd8c7aa8dd4bc6633b446ca
Knifin <angie.t.seattle@gmail.com>
2024-09-02 14:00:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
         origin: "https://app.shinpostudios.com",
-        methods: ["GET", "PUT"]
+        methods: ["GET", "PUT"],
+        credentials: true
     }
 });
 
