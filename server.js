bf8500abbb2bf14e675fe7bac281c0d1e5242d78
Knifin <angie.t.seattle@gmail.com>
2024-09-01 21:33:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,18 @@ import { createServer } from "https";
 import { readFileSync } from "fs";
 import { Server } from "socket.io";
 
+// const httpServer = createServer();
+
 const httpServer = createServer({
   key: readFileSync("/app/tls/server.key"),
-  cert: readFileSync("/app/tls/server.crt"),
-  ca: readFileSync("/app/tls/server.letsencrypt.crt"),
-  rejectUnauthorized: false
+  cert: readFileSync("/app/tls/server.crt")
 });
-
-// const httpServer = createServer();
 
 const io = new Server(httpServer, {
   cors: {
         origin: "https://app.shinpostudios.com",
         methods: ["GET", "POST"]
-    }
+  },
 });
 
 io.on("connection", (socket) => {
