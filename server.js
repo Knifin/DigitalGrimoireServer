51aaca37ecdb67678bb2b0c6aa8536c7ecda9fac
Knifin <angie.t.seattle@gmail.com>
2024-09-02 08:50:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 import express from "express";
-import cors from "cors";
 import { createServer } from "https";
 import { readFileSync } from "fs";
 
 const options = {
     key: readFileSync("/app/tls/server.key"),
-    cert: readFileSync("/app/tls/server.crt"),
-    ca:
-        [readFileSync("/app/tls/server.letsencrypt.crt")]
+    cert: readFileSync("/app/tls/server.crt")
 };
 
 const app = express();
 const httpsServer = createServer(options, app);
-app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
