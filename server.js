30df4410bc5044abe5f25bc38fea8f9460fd641f
Knifin <angie.t.seattle@gmail.com>
2024-09-02 07:44:04 -0700

    Test server.js

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express from "express";
 import cors from "cors";
+import { readFileSync } from "fs";
+import { createServer } from "https";
 
 const app = express();
+
+let options = {
+    key: readFileSync('/app/tls/server.key'),
+    cert: readFileSync('/app/tls/server.crt'),
+};
+
+const httpsServer = createServer(options, app);
+const io = require("socket.io")(httpsServer);
+
 app.use(cors());
 
 app.get('/', (req, res) => {
