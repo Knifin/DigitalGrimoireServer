cc56349aa4bca0ae9b7c36473c55e153283907cb
Knifin <angie.t.seattle@gmail.com>
2024-09-02 20:13:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+import express from "express";
+import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-const httpServer = createServer();
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
         origin: process.env.CLIENT_URL,
@@ -12,6 +16,10 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
     socket.emit("hello", "Hello World from the Server!");
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 httpServer.listen(process.env.PORT || 5000);
